bdab3cfaec3cbcee791c4d30d26eb956a9124427
salaimuniselvam <salaimuniselvams@geakminds.com>
2023-07-10 22:24:46 +0530

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=" grid place-content-center min-h-screen text-3xl">
+        Food App
+      </div>
+    </>
+  );
+}
+
+export default App;
